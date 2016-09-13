417730353b9513dd5e9c4ecac784929ad0bbd281
i5ting <i5ting@126.com>
2016-09-13 09:26:31 +0800

    refact, please ignore

--- config.js
+++ config.js
@@ -9,7 +9,12 @@ module.exports = {
   routes:[{
     path: __dirname,
     folder: 'app/routes'
-  }]
-
-
+  }],
+  
+  database: {
+    'host': '127.0.0.1',
+    'port': '27017',
+    'db': 'moa-api',
+    'is_debug': true
+  }
 }
