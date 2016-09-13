13af32e594031308a3932282dcfacd6acdcce151
i5ting <i5ting@126.com>
2016-09-13 09:33:08 +0800

    refact, please ignore

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ global.debug = require('debug')('moa2');
 var app = require('moa2')(__dirname + '/config')
 
 debug($config)
-debug($middlewares)
 debug($global_middlewares)
+debug($middlewares)
+debug($controllers)
+debug($models)
 
 app.start(4000);
